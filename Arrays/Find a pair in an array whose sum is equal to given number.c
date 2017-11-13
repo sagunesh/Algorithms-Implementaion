01d43eca349ed5274eb46c287b67db8038afc058
Sagunesh Grover <sagunesh@gmail.com>
2017-11-13 06:13:27 +0530

    Find the pair whose sum is X using hash approach
    Find a better approach for same algo so updating

--- Arrays/Find a pair in an array whose sum is equal to given number.c
+++ Arrays/Find a pair in an array whose sum is equal to given number.c
@@ -1,14 +1,44 @@
 
+/*----
+#Hash approach took O(n) time
+#Naive Approach took O(n^2) time
+This can be seen on very large inputs
+*/
+
 #include <stdio.h>
+#include <time.h>
+#define MAX 100
+
 int count;
-void findpair(int* arr,int n,int sum){
+void findpair(int* arr,int size,int sum){
+    //hash approach
+    int i,temp;
+    int hash[MAX]={0};
+    for(i=0;i<size;i++){
+        temp = sum - arr[i];
+        if(temp>=0&&hash[temp]==1){
+             count +=1;
+            printf("The pair found with given sum using hash approach %d is (%d , %d) ",sum,arr[i],temp);
+            printf("\n");
+        }
+        hash[arr[i]]=1;
+    }
+    if(count==0){
+        printf("No pair found whose sum is = %d \n",sum);
+    }else{
+        printf("Total pair found whose sum = %d is %d \n",sum,count);
+
+    }
+}
+//naive approach
+void findpairnaive(int* arr,int n,int sum){
         int i,j,arr_sum;
     for(i=0;i<n;i++){
         for(j=i+1;j<n;j++){
             arr_sum=arr[i]+arr[j];
             if(arr_sum==sum){
                     count +=1;
-            printf("The pair found is (%d , %d) ",arr[i],arr[j]);
+            printf("The pair found using naive approach is (%d , %d) ",arr[i],arr[j]);
             printf("\n");
 
         }
@@ -16,9 +46,9 @@ void findpair(int* arr,int n,int sum){
      }
     }
     if(count==0){
-        printf("No pair found whose sum is = %d",sum);
+        printf("No pair found whose sum is = %d\n",sum);
     }else{
-        printf("Total pair found whose sum = %d is %d",sum,count);
+        printf("Total pair found whose sum = %d is %d\n",sum,count);
 
     }
 
@@ -31,6 +61,10 @@ int main()
     //scanning the elements in an array
     printf("Enter the number of elements in an array\n");
     scanf("%d",&n);
+    if(n>MAX){
+        printf("Array size can't be greater than %d \n",MAX);
+        exit(1);
+    }
     printf("Enter all %d  elements \n",n);
     for(i=0;i<n;i++){
         scanf("%d",&arr[i]);
@@ -46,8 +80,27 @@ int main()
     printf("Enter the element for which pair sum we need to find\n");
     scanf("%d",&sum);
 
-    //calling find pair function
+    //calling find pair function  using hash approach
+    // Calculate the time taken
+
+    // comparison between naive and hash approach
+    clock_t t;
+    t = clock();
     findpair(arr,n,sum);
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+
+    printf("findpair() took %f seconds to execute \n", time_taken);
+
+    clock_t t1;
+    t1 = clock();
+    findpairnaive(arr,n,sum);
+    t1 = clock() - t1;
+    double time_taken1 = ((double)t)/CLOCKS_PER_SEC; // in seconds
+
+    printf("findpairnaive() took %f seconds to execute \n", time_taken1);
+    return 0;
+
 
 
 
